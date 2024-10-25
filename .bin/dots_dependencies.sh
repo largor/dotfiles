eefeb1bcc9c63fb5124d91869fd3a5d8828fffcf
largor <lars.gorczyca@outlook.com>
2024-10-24 23:16:03 -0400

    Updating script to install https://github.com/Jas-SinghFSU/HyprPanel.

--- .bin/dots_dependencies.sh
+++ .bin/dots_dependencies.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env sh
+yay -S grimblast-git gpu-screen-recorder hyprpicker matugen-bin python-gpustat aylurs-gtk-shell-git
 
-pacman -S kvantum fastfetch fuzzel kitty qt5ct qt6ct ranger hypridle hyprlock hyprpaper
+sudo pacman -S kvantum fastfetch fuzzel kitty qt5ct qt6ct ranger hypridle hyprlock hyprpaper pipewire libgtop bluez bluez-utils btop networkmanager dart-sass wl-clipboard brightnessctl swww python gnome-bluetooth-3.0 pacman-contrib power-profiles-daemon
+
+git clone https://github.com/Jas-SinghFSU/HyprPanel.git && \
+  ln -s $(pwd)/HyprPanel $HOME/.config/ags
