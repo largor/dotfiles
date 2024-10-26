868dc58aa1d66e7b72d86968b179d8783264ffbc
largor <lars.gorczyca@outlook.com>
2024-10-25 23:40:28 -0400

    Updating dependencies

--- .bin/dots_dependencies.sh
+++ .bin/dots_dependencies.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 yay -S grimblast-git gpu-screen-recorder hyprpicker matugen-bin python-gpustat aylurs-gtk-shell-git
 
-sudo pacman -S kvantum fastfetch fuzzel kitty qt5ct qt6ct ranger hypridle hyprlock hyprpaper pipewire libgtop bluez bluez-utils btop networkmanager dart-sass wl-clipboard brightnessctl swww python gnome-bluetooth-3.0 pacman-contrib power-profiles-daemon
+sudo pacman -S kvantum fastfetch fuzzel kitty qt5ct qt6ct ranger hypridle hyprlock hyprpaper pipewire libgtop bluez bluez-utils btop networkmanager dart-sass wl-clipboard brightnessctl swww python gnome-bluetooth-3.0 pacman-contrib power-profiles-daemon polkit-gnome
 
 git clone https://github.com/Jas-SinghFSU/HyprPanel.git && \
   ln -s $(pwd)/HyprPanel $HOME/.config/ags
