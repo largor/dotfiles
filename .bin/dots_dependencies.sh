0520b56002e6519b77d78c2988bd063ade47c5ae
largor <lars.gorczyca@outlook.com>
2024-10-26 00:05:40 -0400

    Updating dependencies

--- .bin/dots_dependencies.sh
+++ .bin/dots_dependencies.sh
@@ -4,8 +4,8 @@
 curl -fsSL https://bun.sh/install | bash && \
   sudo ln -s $HOME/.bun/bin/bun /usr/local/bin/bun
 #some dependencies
-sudo pacman -S git emacs vim kvantum fastfetch fuzzel kitty qt5ct qt6ct ranger hypridle hyprlock hyprpaper pipewire libgtop bluez bluez-utils btop networkmanager dart-sass wl-clipboard brightnessctl swww python gnome-bluetooth-3.0 pacman-contrib power-profiles-daemon polkit-gnome
-yay -S grimblast-git gpu-screen-recorder hyprpicker matugen-bin python-gpustat aylurs-gtk-shell-git
+sudo pacman -S git emacs vim kvantum fastfetch fuzzel kitty qt5ct qt6ct ranger hypridle hyprlock hyprpaper pipewire libgtop bluez bluez-utils btop networkmanager dart-sass wl-clipboard brightnessctl swww python gnome-bluetooth-3.0 pacman-contrib power-profiles-daemon polkit-gnome fortune-mod cowsay
+yay -S grimblast-git gpu-screen-recorder hyprpicker matugen-bin python-gpustat aylurs-gtk-shell-git pokeget
 #installing HyprPanel
 git clone https://github.com/Jas-SinghFSU/HyprPanel.git && \
   ln -s $(pwd)/HyprPanel $HOME/.config/ags
