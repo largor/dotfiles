2ba555d2c80893baf678f6d51979b07e0b215c9d
largor <lars.gorczyca@outlook.com>
2024-11-16 01:40:54 -0500

    Update install_dots.sh

--- .bin/install_dots.sh
+++ .bin/install_dots.sh
@@ -22,7 +22,7 @@ sudo pacman -S emacs vim kvantum fastfetch\
 	steam unzip ark texlive spotify-launcher audiocd-kio\
 	gnome-disk-utility dosfstools ntfsprogs mopidy\
 	python-pip nwg-look nwg-displays ispell libreoffice-still\
- 	retroarch calibre sdcv tmux
+ 	retroarch calibre sdcv tmux man
 
 yay -S grimblast-git gpu-screen-recorder hyprpicker\
 	matugen-bin python-gpustat aylurs-gtk-shell-git\
