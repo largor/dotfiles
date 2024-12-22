c3a70a30384c34eeeb222ebc2c43345be3b9d844
largor <lars.gorczyca@outlook.com>
2024-12-21 23:47:37 -0500

    Update install_dots.sh
    updating dependencies and HyprPanel Install

--- .bin/install_dots.sh
+++ .bin/install_dots.sh
@@ -10,39 +10,31 @@
 #ranger & dolphin used for file managers.
 #mpd & ncmpcpp as music player.  Mopidy, mopidy-mpd, and mopidy-subidy allow ncmpcpp to connect to my navidrome server.
 #mpv for video player.
-sudo pacman -S emacs vim kvantum fastfetch\
-	fuzzel kitty qt5ct qt6ct ranger hypridle\
-	hyprlock hyprpaper pipewire libgtop bluez\
-	bluez-utils btop networkmanager dart-sass\
-	wl-clipboard brightnessctl swww python\
-	gnome-bluetooth-3.0 pacman-contrib\
-	power-profiles-daemon polkit-gnome\
-	fortune-mod cowsay python-pillow pkgfile firefox\
-	keepassxc mpv mpd ncmpcpp feh dolphin qbittorrent\
-	steam unzip ark texlive spotify-launcher audiocd-kio\
-	gnome-disk-utility dosfstools ntfsprogs mopidy\
-	python-pip nwg-look nwg-displays ispell libreoffice-still\
- 	retroarch calibre sdcv tmux man ttf-nerd-fonts-symbols\
-  	blueberry
+sudo pacman -S wireplumber libgtop bluez bluez-utils\
+	btop networkmanager dart-sass wl-clipboard brightnessctl\
+ 	swww python upower pacman-contrib power-profiles-daemon\
+  	gvfs emacs vim kvantum fastfetch fuzzel kitty qt5ct qt6ct\
+   	ranger hyprlock hyprpaper pipewire polkit-gnome	fortune-mod\
+    	cowsay python-pillow pkgfile firefox keepassxc mpv mpd ncmpcpp\
+     	feh dolphin qbittorrent	steam unzip ark texlive spotify-launcher\
+      	audiocd-kio gnome-disk-utility dosfstools ntfsprogs mopidy python-pip\
+       	nwg-look nwg-displays ispell libreoffice-still retroarch calibre sdcv tmux\
+	man ttf-nerd-fonts-symbols blueberry
 
-yay -S grimblast-git gpu-screen-recorder hyprpicker\
-	matugen-bin python-gpustat aylurs-gtk-shell-git\
-	pokeget openssl-1.0 rclone-browser\
-	heroic-games-launcher-bin nicotine-plus\
-	jellyfin-media-player mopidy-mpd mopidy-subidy\
-	brave-bin signal-desktop catppuccin-gtk-theme-macchiato\
- 	papirus-folders-catppuccin-git downgrade cava libretro\
-  	epy-ereader-git nerd-fonts-git
-
-#Bun is dependency for HyprPanel
-curl -fsSL https://bun.sh/install | bash && \
-  sudo ln -s $HOME/.bun/bin/bun /usr/local/bin/bun
+yay -S aylurs-gtk-shell-git grimblast-git gpu-screen-recorder-git hyprpicker matugen-bin\
+	python-gpustat hyprsunset-git hypridle-git pokeget openssl-1.0 rclone-browser\
+	heroic-games-launcher-bin nicotine-plus jellyfin-media-player mopidy-mpd mopidy-subidy\
+	brave-bin signal-desktop catppuccin-gtk-theme-macchiato papirus-folders-catppuccin-git\
+ 	downgrade cava libretro	epy-ereader-git nerd-fonts-git
 
 #installing HyprPanel
 mkdir $HOME/.bin
 git clone https://github.com/Jas-SinghFSU/HyprPanel.git $HOME/.bin && \
-  ln -s $HOME/.bin/HyprPanel $HOME/.config/ags
-$HOME/.config/ags/install_fonts.sh
+cd HyprPanel
+meson setup build
+meson compile -C build
+meson install -C build
+$HOME/.bin/HyprPanel/scripts/install_fonts.sh
 
 #installing doom emacs
 git clone --depth 1 https://github.com/doomemacs/doomemacs ~/.config/emacs
