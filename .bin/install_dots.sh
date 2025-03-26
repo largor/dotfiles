c5beac8b702659b040d10c4d7df264fd3229641b
largor <lars.gorczyca@outlook.com>
2025-03-25 22:43:49 -0400

    Update install_dots.sh
    updating dependencies - replaced ranger with yazi & removing Hyprbar

--- .bin/install_dots.sh
+++ .bin/install_dots.sh
@@ -7,36 +7,26 @@
 #hypridle, hyprlock, hyprpaper, and grimblast are a part of the hyprland eco-system.
 #gnome-disk-utility is a handy tool for managing disks.
 #dosfstools and ntfsprogs for formating usbs to be compatible with Windows systems (required for use with gnome-disk-utility)
-#ranger & dolphin used for file managers.
+#yazi & dolphin used for file managers.
 #mpd & ncmpcpp as music player.  Mopidy, mopidy-mpd, and mopidy-subidy allow ncmpcpp to connect to my navidrome server.
 #mpv for video player.
 sudo pacman -S wireplumber libgtop bluez bluez-utils\
 	btop networkmanager dart-sass wl-clipboard brightnessctl\
  	swww python upower pacman-contrib power-profiles-daemon\
   	gvfs emacs vim kvantum fastfetch fuzzel kitty qt5ct qt6ct\
-   	ranger hyprlock hyprpaper pipewire polkit-gnome	fortune-mod\
+   	hyprlock hyprpaper pipewire polkit-gnome fortune-mod\
     	cowsay python-pillow pkgfile firefox keepassxc mpv mpd ncmpcpp\
      	feh dolphin qbittorrent	steam unzip ark texlive spotify-launcher\
       	audiocd-kio gnome-disk-utility dosfstools ntfsprogs mopidy python-pip\
        	nwg-look nwg-displays ispell libreoffice-still retroarch calibre sdcv tmux\
 	man ttf-nerd-fonts-symbols blueberry meson waybar pavucontrol code cliphist\
- 	wl-clip-persist ueberzugpp
+ 	wl-clip-persist yazi
 
-yay -S aylurs-gtk-shell-git grimblast-git gpu-screen-recorder-git hyprpicker matugen-bin\
+yay -S grimblast-git gpu-screen-recorder-git hyprpicker matugen-bin wlogout\
 	python-gpustat hyprsunset-git hypridle-git pokeget openssl-1.0 rclone-browser\
 	heroic-games-launcher-bin nicotine-plus jellyfin-media-player mopidy-mpd mopidy-subidy\
 	brave-bin signal-desktop catppuccin-gtk-theme-macchiato papirus-folders-catppuccin-git\
  	downgrade cava libretro	epy-ereader-git hyprland-qtutils catppuccin-cursors-macchiato\
-  	wlogout
-
-# Install HyprPanel.  Uncommented for now while using Waybar
-# mkdir $HOME/.bin
-# git clone https://github.com/Jas-SinghFSU/HyprPanel.git $HOME/.bin && \
-# cd HyprPanel
-# meson setup build
-# meson compile -C build
-# meson install -C build
-# $HOME/.bin/HyprPanel/scripts/install_fonts.sh
 
 #installing doom emacs
 git clone --depth 1 https://github.com/doomemacs/doomemacs ~/.config/emacs
